43626fe80f21f52e148919ac98c54c506f400e27
alenharta <alenharta@alenhartas-air.myfiosgateway.com>
2020-08-11 11:29:42 -0400

    final commits

--- flask_learning_journal_v3/.gitignore/venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/alenharta/techdegree_project_5/flask_learning_journal_v3/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
---
+++ untitled/test.py
@@ -0,0 +1,3 @@
+total = 2.98 + 15.72
+print(total-(total*(15/100)))
+help(str.rjust)
